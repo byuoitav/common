5246bbaa15a3b96709bff6c7fd3f0b18fae1eb20
jpw547 <jwerner547@gmail.com>
2018-11-27 10:37:06 -0700

    hardware struct

---
+++ structs/hardware.go
@@ -0,0 +1,12 @@
+package structs
+
+// HardwareInfo contains the common information for device hardware information
+type HardwareInfo struct {
+	IPAddress       string `json:"ip_address,omitempty"`
+	MACAddress      string `json:"mac_address,omitempty"`
+	SerialNumber    string `json:"serial_number,omitempty"`
+	FirmwareVersion string `json:"firmware_version,omitempty"`
+	FilterStatus    string `json:"filter_status,omitempty"`
+	WarningStatus   string `json:"warning_status,omitempty"`
+	ErrorStatus     string `json:"error_status,omitempty"`
+}
