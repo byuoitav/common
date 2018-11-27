004c74f68ae9060574cc849b018f28c41408dbc1
jpw547 <jwerner547@gmail.com>
2018-11-27 12:34:11 -0700

    changes to the hardware struct

--- structs/hardware.go
+++ structs/hardware.go
@@ -2,6 +2,7 @@ package structs
 
 // HardwareInfo contains the common information for device hardware information
 type HardwareInfo struct {
+	Hostname        string      `json:"hostname,omitempty"`
 	ModelName       string      `json:"model_name,omitempty"`
 	IPAddress       string      `json:"ip_address,omitempty"`
 	MACAddress      string      `json:"mac_address,omitempty"`
@@ -10,4 +11,6 @@ type HardwareInfo struct {
 	FilterStatus    string      `json:"filter_status,omitempty"`
 	WarningStatus   []string    `json:"warning_status,omitempty"`
 	ErrorStatus     []string    `json:"error_status,omitempty"`
+	PowerStatus     string      `json:"power_status,omitempty"`
+	TimerInfo       interface{} `json:"timer_info"`
 }
