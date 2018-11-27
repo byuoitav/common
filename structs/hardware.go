7bb1ab287c774b16edca282bf2a085053e65f990
jpw547 <jwerner547@gmail.com>
2018-11-27 14:41:43 -0700

    changing the network reporting structure

--- structs/hardware.go
+++ structs/hardware.go
@@ -4,13 +4,19 @@ package structs
 type HardwareInfo struct {
 	Hostname        string      `json:"hostname,omitempty"`
 	ModelName       string      `json:"model_name,omitempty"`
-	IPAddress       string      `json:"ip_address,omitempty"`
-	MACAddress      string      `json:"mac_address,omitempty"`
 	SerialNumber    string      `json:"serial_number,omitempty"`
 	FirmwareVersion interface{} `json:"firmware_version,omitempty"`
+	NetworkInfo     NetworkInfo `json:"network_information,omitempty"`
 	FilterStatus    string      `json:"filter_status,omitempty"`
 	WarningStatus   []string    `json:"warning_status,omitempty"`
 	ErrorStatus     []string    `json:"error_status,omitempty"`
 	PowerStatus     string      `json:"power_status,omitempty"`
-	TimerInfo       interface{} `json:"timer_info"`
+	TimerInfo       interface{} `json:"timer_info,omitempty"`
+}
+
+// NetworkInfo contains the network information for the device
+type NetworkInfo struct {
+	IPAddress  string   `json:"ip_address,omitempty"`
+	MACAddress string   `json:"mac_address,omitempty"`
+	DNS        []string `json:"dns,omitempty"`
 }
