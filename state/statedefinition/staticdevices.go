1e5fcb62725e6bc1d2acac917d8abeab1a22fd2f
Joseph Blodgett <joseph.blodgett@gmail.com>
2019-02-15 16:19:56 -0700

    lamp hours and temperature are integer values

--- state/statedefinition/staticdevices.go
+++ state/statedefinition/staticdevices.go
@@ -51,8 +51,8 @@ type StaticDevice struct {
 	//Display Specific Fields
 	Blanked      *bool  `json:"blanked,omitempty"`
 	Input        string `json:"input,omitempty"`
-	LampHours    string `json:"lamp-hours,omitempty"`
-	Temperature  string `json:"temperature,omitempty"`
+	LampHours    *int   `json:"lamp-hours,omitempty"`
+	Temperature  *int   `json:"temperature,omitempty"`
 	ActiveSignal *bool  `json:"active-signal,omitempty"`
 
 	//Audio Device Specific Fields
