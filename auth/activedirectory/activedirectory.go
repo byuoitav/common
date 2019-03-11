322931a44c153eaf66cba09baf9401da0f3c2962
jpw547 <jwerner547@gmail.com>
2019-03-11 12:27:56 -0600

    re-working some active directory stuff and getting responder information

--- auth/activedirectory/activedirectory.go
+++ auth/activedirectory/activedirectory.go
@@ -17,13 +17,14 @@ func init() {
 	ldapUsername = os.Getenv("LDAP_USERNAME")
 	ldapPassword = os.Getenv("LDAP_PASSWORD")
 	ldapURL = os.Getenv("LDAP_URL")
-	ldapSearchScope := os.Getenv("LDAP_SEARCH_SCOPE")
+	ldapSearchScope = os.Getenv("LDAP_SEARCH_SCOPE")
 
 	if len(ldapUsername) == 0 || len(ldapPassword) == 0 || len(ldapURL) == 0 || len(ldapSearchScope) == 0 {
 		log.L.Fatalf("LDAP username, password, search scope, or URL not set.")
 	}
 }
 
+// GetGroupsForUser gets the groups for a user
 func GetGroupsForUser(user string) ([]string, *nerr.E) {
 	var groups []string
 	// connect to ldap server
@@ -54,7 +55,7 @@ func GetGroupsForUser(user string) ([]string, *nerr.E) {
 		0,
 		false,
 		fmt.Sprintf("(name=%s)", user),
-		[]string{"name", "memberOf"},
+		[]string{"name", "memberOf", "displayName"},
 		nil,
 	)
 
@@ -63,17 +64,19 @@ func GetGroupsForUser(user string) ([]string, *nerr.E) {
 		return groups, nerr.Translate(err).Addf("failed to search ldap: %s")
 	}
 
+	sr.PrettyPrint(5)
+
 	for _, entry := range sr.Entries {
 		if strings.EqualFold(user, entry.GetAttributeValue("name")) {
 			tmp := entry.GetAttributeValues("memberOf")
-			groups = translateAttributes(tmp)
+			groups = translateAttributes(tmp, true)
 			break
 		}
 	}
 	return groups, nil
 }
 
-func translateAttributes(attributes []string) []string {
+func translateAttributes(attributes []string, writeOU bool) []string {
 	var paths []string
 
 	for _, attribute := range attributes {
@@ -90,8 +93,10 @@ func translateAttributes(attributes []string) []string {
 			case "DC":
 				break
 			case "OU":
-				path.WriteString(v[1])
-				path.WriteString("/")
+				if writeOU {
+					path.WriteString(v[1])
+					path.WriteString("/")
+				}
 			case "CN":
 				paths = append(paths, path.String()+v[1])
 			}
@@ -108,4 +113,79 @@ func reverseStringSlice(s []string) []string {
 	}
 
 	return s
+}
+
+// GetUsersByGroups returns a list of users based on the one or more group names that are given.
+func GetUsersByGroups(group ...string) ([]string, *nerr.E) {
+	groups := []string{}
+
+	// connect to ldap server
+	l, err := ldap.Dial("tcp", ldapURL)
+	if err != nil {
+		return groups, nerr.Translate(err).Addf("Unable to dial LDAP to get groups.")
+	}
+	defer l.Close()
+
+	// connect with tls
+	err = l.StartTLS(&tls.Config{InsecureSkipVerify: true})
+	if err != nil {
+		return groups, nerr.Translate(err).Addf("unable to connect to active directory with tls.")
+	}
+
+	// bind with user/pass
+	err = l.Bind(ldapUsername, ldapPassword)
+	if err != nil {
+		return groups, nerr.Translate(err).Addf("unable to bind username/password to ldap connection")
+	}
+
+	log.L.Info("\nBOUND\n")
+
+	// build the search request
+	searchRequest := ldap.NewSearchRequest(
+		ldapSearchScope,
+		ldap.ScopeWholeSubtree,
+		ldap.DerefAlways,
+		0,
+		0,
+		false,
+		fmt.Sprintf("(name=%s)", group[0]),
+		[]string{},
+		nil,
+	)
+
+	sr, err := l.Search(searchRequest)
+	if err != nil {
+		return groups, nerr.Translate(err).Addf("failed to search ldap: %s")
+	}
+
+	// sr.PrettyPrint(4)
+	// fmt.Printf("%+v", sr.Entries[0].GetAttributeValues("member")[0])
+
+	people := translateAttributes(sr.Entries[0].GetAttributeValues("member"), false)
+
+	// fmt.Printf("%s", people)
+
+	for _, netID := range people {
+		// build the search request
+		searchRequest := ldap.NewSearchRequest(
+			ldapSearchScope,
+			ldap.ScopeWholeSubtree,
+			ldap.DerefAlways,
+			0,
+			0,
+			false,
+			fmt.Sprintf("(name=%s)", netID),
+			[]string{"name", "displayName"},
+			nil,
+		)
+
+		nameResult, err := l.Search(searchRequest)
+		if err != nil {
+			return groups, nerr.Translate(err).Addf("failed to search ldap: %s")
+		}
+
+		fmt.Printf("%s\n", nameResult.Entries[0].GetAttributeValue("displayName"))
+	}
+
+	return []string{}, nil
 }
--- structs/person.go
+++ structs/person.go
@@ -1,138 +1,138 @@
 package structs
 
-import "time"
-
-// Auth - our authentication struct.
+// Person - our authentication struct.
 type Person struct {
-	Basic struct {
-		Links struct {
-			BasicInfo struct {
-				Rel    string `json:"rel"`
-				Href   string `json:"href"`
-				Method string `json:"method"`
-			} `json:"basic__info"`
-			BasicModify struct {
-				Rel    string `json:"rel"`
-				Href   string `json:"href"`
-				Method string `json:"method"`
-			} `json:"basic__modify"`
-		} `json:"links"`
-		Metadata struct {
-			Restricted         bool `json:"restricted"`
-			ValidationResponse struct {
-				Code    int    `json:"code"`
-				Message string `json:"message"`
-			} `json:"validation_response"`
-			ValidationInformation []string `json:"validation_information"`
-		} `json:"metadata"`
-		UpdatedDatetime struct {
-			Value   time.Time `json:"value"`
-			APIType string    `json:"api_type"`
-		} `json:"updated_datetime"`
-		UpdatedByByuID struct {
-			Value       string `json:"value"`
-			Description string `json:"description"`
-			APIType     string `json:"api_type"`
-		} `json:"updated_by_byu_id"`
-		CreatedDatetime struct {
-			Value   time.Time `json:"value"`
-			APIType string    `json:"api_type"`
-		} `json:"created_datetime"`
-		CreatedByByuID struct {
-			Value       string `json:"value"`
-			Description string `json:"description"`
-			APIType     string `json:"api_type"`
-		} `json:"created_by_byu_id"`
-		ByuID struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-			Key     bool   `json:"key"`
-		} `json:"byu_id"`
-		PersonID struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"person_id"`
-		NetID struct {
-			Value           string `json:"value"`
-			APIType         string `json:"api_type"`
-			RelatedResource string `json:"related_resource"`
-		} `json:"net_id"`
-		PersonalEmailAddress struct {
-			Value           string `json:"value"`
-			APIType         string `json:"api_type"`
-			RelatedResource string `json:"related_resource"`
-		} `json:"personal_email_address"`
-		PrimaryPhoneNumber struct {
-			Value           interface{} `json:"value"`
-			APIType         string      `json:"api_type"`
-			RelatedResource string      `json:"related_resource"`
-		} `json:"primary_phone_number"`
-		Deceased struct {
-			Value           bool   `json:"value"`
-			APIType         string `json:"api_type"`
-			RelatedResource string `json:"related_resource"`
-		} `json:"deceased"`
-		Sex struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"sex"`
-		FirstName struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"first_name"`
-		MiddleName struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"middle_name"`
-		Surname struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"surname"`
-		Suffix struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"suffix"`
-		PreferredFirstName struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"preferred_first_name"`
-		PreferredSurname struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"preferred_surname"`
-		RestOfName struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"rest_of_name"`
-		NameLnf struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"name_lnf"`
-		NameFnf struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"name_fnf"`
-		PreferredName struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"preferred_name"`
-		HomeTown struct {
-			Value   string `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"home_town"`
-		HomeStateCode struct {
-			Value       string `json:"value"`
-			Description string `json:"description"`
-			APIType     string `json:"api_type"`
-		} `json:"home_state_code"`
-		HomeCountryCode struct {
-			Value       string `json:"value"`
-			Description string `json:"description"`
-			APIType     string `json:"api_type"`
-		} `json:"home_country_code"`
-		MergeInProcess struct {
-			Value   bool   `json:"value"`
-			APIType string `json:"api_type"`
-		} `json:"merge_in_process"`
-	} `json:"basic"`
+	ID   string `json:"net-id"`
+	Name string `json:"name"`
+	// Basic struct {
+	// 	Links struct {
+	// 		BasicInfo struct {
+	// 			Rel    string `json:"rel"`
+	// 			Href   string `json:"href"`
+	// 			Method string `json:"method"`
+	// 		} `json:"basic__info"`
+	// 		BasicModify struct {
+	// 			Rel    string `json:"rel"`
+	// 			Href   string `json:"href"`
+	// 			Method string `json:"method"`
+	// 		} `json:"basic__modify"`
+	// 	} `json:"links"`
+	// 	Metadata struct {
+	// 		Restricted         bool `json:"restricted"`
+	// 		ValidationResponse struct {
+	// 			Code    int    `json:"code"`
+	// 			Message string `json:"message"`
+	// 		} `json:"validation_response"`
+	// 		ValidationInformation []string `json:"validation_information"`
+	// 	} `json:"metadata"`
+	// 	UpdatedDatetime struct {
+	// 		Value   time.Time `json:"value"`
+	// 		APIType string    `json:"api_type"`
+	// 	} `json:"updated_datetime"`
+	// 	UpdatedByByuID struct {
+	// 		Value       string `json:"value"`
+	// 		Description string `json:"description"`
+	// 		APIType     string `json:"api_type"`
+	// 	} `json:"updated_by_byu_id"`
+	// 	CreatedDatetime struct {
+	// 		Value   time.Time `json:"value"`
+	// 		APIType string    `json:"api_type"`
+	// 	} `json:"created_datetime"`
+	// 	CreatedByByuID struct {
+	// 		Value       string `json:"value"`
+	// 		Description string `json:"description"`
+	// 		APIType     string `json:"api_type"`
+	// 	} `json:"created_by_byu_id"`
+	// 	ByuID struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 		Key     bool   `json:"key"`
+	// 	} `json:"byu_id"`
+	// 	PersonID struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"person_id"`
+	// 	NetID struct {
+	// 		Value           string `json:"value"`
+	// 		APIType         string `json:"api_type"`
+	// 		RelatedResource string `json:"related_resource"`
+	// 	} `json:"net_id"`
+	// 	PersonalEmailAddress struct {
+	// 		Value           string `json:"value"`
+	// 		APIType         string `json:"api_type"`
+	// 		RelatedResource string `json:"related_resource"`
+	// 	} `json:"personal_email_address"`
+	// 	PrimaryPhoneNumber struct {
+	// 		Value           interface{} `json:"value"`
+	// 		APIType         string      `json:"api_type"`
+	// 		RelatedResource string      `json:"related_resource"`
+	// 	} `json:"primary_phone_number"`
+	// 	Deceased struct {
+	// 		Value           bool   `json:"value"`
+	// 		APIType         string `json:"api_type"`
+	// 		RelatedResource string `json:"related_resource"`
+	// 	} `json:"deceased"`
+	// 	Sex struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"sex"`
+	// 	FirstName struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"first_name"`
+	// 	MiddleName struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"middle_name"`
+	// 	Surname struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"surname"`
+	// 	Suffix struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"suffix"`
+	// 	PreferredFirstName struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"preferred_first_name"`
+	// 	PreferredSurname struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"preferred_surname"`
+	// 	RestOfName struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"rest_of_name"`
+	// 	NameLnf struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"name_lnf"`
+	// 	NameFnf struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"name_fnf"`
+	// 	PreferredName struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"preferred_name"`
+	// 	HomeTown struct {
+	// 		Value   string `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"home_town"`
+	// 	HomeStateCode struct {
+	// 		Value       string `json:"value"`
+	// 		Description string `json:"description"`
+	// 		APIType     string `json:"api_type"`
+	// 	} `json:"home_state_code"`
+	// 	HomeCountryCode struct {
+	// 		Value       string `json:"value"`
+	// 		Description string `json:"description"`
+	// 		APIType     string `json:"api_type"`
+	// 	} `json:"home_country_code"`
+	// 	MergeInProcess struct {
+	// 		Value   bool   `json:"value"`
+	// 		APIType string `json:"api_type"`
+	// 	} `json:"merge_in_process"`
+	// } `json:"basic"`
 }
