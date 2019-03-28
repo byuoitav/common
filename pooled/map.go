164fd19a1cf8a6e84ba4da4697dbe65128b62612
Daniel Randall <dgrandall@me.com>
2019-03-28 15:46:13 -0600

    pooled connection:

---
+++ pooled/map.go
@@ -0,0 +1,95 @@
+package pooled
+
+import (
+	"net"
+	"sync"
+	"time"
+)
+
+// NewConnection .
+type NewConnection func(key interface{}) (net.Conn, error)
+
+// Work .
+type Work func(net.Conn) error
+
+// Map .
+type Map struct {
+	newConn NewConnection
+	ttl     time.Duration
+
+	m  map[interface{}]chan request
+	mu sync.Mutex
+}
+
+type request struct {
+	work Work
+	resp chan error
+}
+
+// NewMap .
+func NewMap(ttl time.Duration, newConn NewConnection) *Map {
+	return &Map{
+		m:       make(map[interface{}]chan request),
+		newConn: newConn,
+		ttl:     ttl,
+	}
+}
+
+// Do .
+func (m *Map) Do(key interface{}, work Work) error {
+	var reqs chan request
+	var ok bool
+
+	m.mu.Lock()
+	if reqs, ok = m.m[key]; !ok {
+		// open a new connection
+		conn, err := m.newConn(key)
+		if err != nil {
+			m.mu.Unlock()
+			return err
+		}
+
+		reqs = make(chan request, 10)
+		m.mu.Unlock()
+
+		go func() {
+			defer func() {
+				// finish up remaining requests
+				for req := range reqs {
+					req.resp <- req.work(conn)
+				}
+
+				conn.Close()
+			}()
+
+			timer := time.NewTimer(m.ttl)
+
+			for {
+				select {
+				case req := <-reqs:
+					// reset the timer
+					if !timer.Stop() {
+						<-timer.C
+					}
+					timer.Reset(m.ttl)
+
+					req.resp <- req.work(conn)
+				case <-timer.C:
+					m.mu.Lock()
+					delete(m.m, key)
+					m.mu.Unlock()
+					return
+				}
+			}
+		}()
+	}
+
+	m.mu.Lock()
+	req := request{
+		work: work,
+		resp: make(chan error),
+	}
+
+	reqs <- req
+	return <-req.resp
+}
